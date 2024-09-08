8aa4f8f5a395b32df7d0db6c3101957974f5d7f5
Josh <Josh@DESKTOP-A1K7BP3>
2024-09-08 15:54:40 +0800

    added project page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProfilePage from './components/ProfilePage';
 import PhotographyPage from './components/PhotographyPage';
+import ProjectsPage from './components/ProjectsPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ProfilePage />} />
         <Route path="/photography" element={<PhotographyPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
       </Routes>
     </Router>
   );
